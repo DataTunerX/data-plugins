bb2041832b63f8aef226fab00baa799bbbc287ea
LronDC <lun.su@daocloud.io>
2023-11-07 15:51:46 +0800

    initial commit: operator-sdk init & create api
    Feat: Reconcile ops: apply plugin.yaml by spec.datasetClass field
    
    Signed-off-by: LronDC <lun.su@daocloud.io>
    
    Feat: make complete notify url env var and replace plugin.yaml's content when apply
    
    Signed-off-by: LronDC <lun.su@daocloud.io>

---
+++ doc/README-zh.md
@@ -0,0 +1,152 @@
+# 如何使用自己的插件
+
+[TOC]
+
+## 插件的定义
+
+插件在 ***DataTunerX*** 中有两类：***DataPlugin*** 和 ***ScoringPlugin***。本说明针对 ***DataPlugin*** 介绍制作数据集插件的标准，其中包括**如何定义插件**，**如何组织插件的目录结构**，**如何将插件投入使用**等内容。
+
+***DataPlugin*** 是处理数据集的插件。当用户需要对元数据做进一步处理来满足微调需求时，可以使用自己制作的插件来完成数据处理这一步骤，最后将处理好后的数据集信息通过接口传送给 ***dataset-server*** ， 完成 ***Dataset*** 数据的更新。
+
+## 插件的标准
+
+想要插件被 ***DataTunerX*** 识别并成功输出结果，需要遵循下面的标准步骤。
+
+### 第一步
+
+克隆项目到本地（如果您希望您的插件可以加入进官方仓库，则需要先 *fork* 本项目，通过提交个人 *pull request* 的方法提交您的插件），假设您已经 *fork* 了本项目，进入项目目录：
+
+```shell
+~: git clone git@github.com:<Your github id>/data-plugins.git
+~: cd data-plugins
+```
+
+在 `/data-plugins` 目录下创建自己的目录，目录的名称视为插件的 ***Provider*** 。***Provider*** 定义为插件的供应商，可以是您公司、组织甚至个人的名称，来标识提供本插件的供应者是谁。进入 “***Provider***” 目录再创建第二层目录，第二层目录的名称视为插件的 ***DatasetClass***。***DatasetClass*** 定义为此数据集插件的类型，如插件是以 *Argo Workflow* 的方式运行，则 ***DatasetClass*** 指明 `Argo Workflow`：
+
+```shell
+/data-plugins: mkdir sample-provider && cd sample-provider
+/sample-provider: mkdir sample-dataset-class && cd sample-dataset-class
+```
+
+```shell
+/data-plugins: tree -L 3
+.
+├── LICENSE
+├── README.md
+└── sample-provider
+    └── sample-dataset-class
+        └── plugin.yaml
+
+3 directories, 3 files
+```
+
+### 第二步
+
+在创建好 “***DatasetClass***” 目录后，您必须在目录下加入一个名为 `plugin.yaml` 的 *k8s* 常规资源对象（或已经在环境安装了相关 *CRD* 的对象）的编排文件模板。`plugin.yaml` 模板遵循 ***Go text/template*** 标准。***Dataset Controller*** 会对 `plugin.yaml` 模板中的变量（如果存在）进行替换然后下发（相关逻辑请见[附加说明](#附加说明)），相关资源对象也就是插件会被创建并启动：
+
+```shell
+/sample-dataset-class: ls
+plugin.yaml
+/sample-dataset-class: cat plugin.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: {{ .Name }}
+spec:
+  replicas: {{ .Replicas }}
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+        - name: nginx
+          image: {{ .Image }}
+          resources:
+            limits:
+              memory: "128Mi"
+              cpu: "200m"
+            requests:
+              memory: "64Mi"
+              cpu: "100m"
+```
+
+#### 附加说明
+
+***DataTunerX*** 支持向插件提供静态固定参数和动态运行时参数，这基于 ***Dataset*** *CRD* 和 ***DataPlugin*** *CRD* 的定义。插件制作者可以在 “***DatasetClass***” 目录下同时新建一个描述本插件的 ***DataPlugin*** *CR* 编排文件 `dataplugin.yaml` 随插件包一同提交，也可以直接部署在自己的安装好 ***DataTunerX*** 的集群上，`dataplugin.yaml` 的格式如下：
+
+```yaml
+apiVersion: extension.datatunerx.io/v1beta1
+kind: DataPlugin
+metadata:
+  name: <plugin name>
+  namespace: <datatunerx system namespace>
+spec:
+  provider: <provider code>
+  datasetClass: <dataset class code>
+  version: <version code>
+  parameters: [plugin static fixed parameters in json string format]
+```
+
+- Name：指插件的名称
+- Namespace：插件会和 ***DataTunerX*** 组件安装在同一个命名空间下面，如 `datatunerx-system`
+- Provider：和插件第一层 “***Provider***” 目录同名
+- DatasetClass：和插件第二层 “***DatasetClass***” 目录同名，`provider` 和 `datasetClass` 共同指引 ***Dataset Controller*** 应该去哪个路径下寻找 `plugin.yaml` 文件
+- Version：描述插件版本
+- Parameters：***DataPlugin*** *CR* 中定义的 `parameters` 字段为插件的静态固定参数，需要遵循 *Json string* 格式，如 `'{"param1": "value1", "param2": "2", "param3": "true"}'` 。***Dataset Controller*** 在启动插件时会先用 ***DataPlugin*** *CR* 中的 `parameters` 替换一次 `plugin.yaml` 模板中的值，再读取创建 ***Dataset*** *CR* 时用户写入的动态运行时参数（两种 **Parameters** 使用方法相同 ），覆盖替换一次 `plugin.yaml` 模板中的值，也就是说如果 ***Parameters*** 中有同一个参数字段，则最终生效的是创建 ***Dataset*** *CR* 时写入的动态运行时参数。另外，两种 ***Parameters*** 都可以为空，但需要插件内部逻辑中处理好没有给出任何参数的情况。
+
+### 最后一步
+
+再插件完成数据处理工作后，需要调用**通知接口**告知处理完成的数据集相关信息。接口地址由 ***Dataset Controller*** 生成 `CompleteNotifyUrl` 变量并添加进 ***Parameters*** 中，以便在应用 `plugin.yaml` 时替换相应的变量值。所以此步骤要求插件**务必**实现 `CompleteNotifyUrl` 变量的识别，并在 `plugin.yaml` 中预留好 `CompleteNotifyUrl` 的位置，如：
+
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: {{ .Name }}
+spec:
+  replicas: {{ .Replicas }}
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+        - name: nginx
+          image: {{ .Image }}
+          env:
+          - name: COMPLETE_NOTIFY_URL
+            value: {{ .ComplateNotifyUrl }}
+...
+```
+
+**通知接口**接受 *HTTP POST* 方法请求，需要传递的 *Request Body* 为：
+
+```json
+[
+    {
+        "Name": "Default",  // Dataset subset's name, e.g., Random Sample Subset, Balanced Class Subset, Time Window Subset, Feature Subset, Cross-Validation Subset, Outlier Detection Subset, etc. Default value is "Default" if not specified.
+        "Splits": {
+            "Train": {
+                "File": "s3://bucket/training_data.csv",  // Training Dataset's address. This can be an S3 protocol address, indicating the location of the training data file on an S3 bucket.
+            },
+            "Test": {
+                "File": "s3://bucket/testing_data.csv",  // Testing Dataset's address. Similar to the training data address, this can be an S3 protocol address.
+            },
+            "Validate": {
+                "File": "s3://bucket/validation_data.csv",  // Validation Dataset's address. Similar to the training data address, this can be an S3 protocol address.
+            }
+        }
+    }
+]
+```
+
+数据集包含所有可用的数据，可以根据特定规则或目的选择一部分数据当作子集，子集进一步拆分为训练集、测试集、验证集。插件在处理好这样的数据结构后，将数据集信息传递到**通知接口**，更新 ***Dataset*** 信息，***Dataset*** 为可用状态。
+
+至此，您已完成制作和使用 ***DataTunerX*** 数据集插件 ***DataPlugin*** 的所有步骤。
---
+++ internal/controller/dataplugin_controller.go
@@ -0,0 +1,258 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/DataTunerX/data-plugins/pkg/config"
+	extensionv1beta1 "github.com/DataTunerX/meta-server/api/extension/v1beta1" // import DataPlugin API
+	"github.com/DataTunerX/utility-server/logging"
+	"github.com/DataTunerX/utility-server/parser"
+	"github.com/qiniu/x/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+// DataPluginReconciler reconciles a DataPlugin object
+type DataPluginReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Log    logging.Logger
+}
+
+//+kubebuilder:rbac:groups=extension.datatunerx.io,resources=dataplugins,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=extension.datatunerx.io,resources=dataplugins/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=extension.datatunerx.io,resources=dataplugins/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the DataPlugin object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+
+// Reconcile reads that state of the cluster for a Dataset object and makes changes based on the state read
+// and what is in the DataPlugin.Spec
+func (r *DataPluginReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	r.Log.Info("Reconciling Dataset")
+
+	// Fetch the Dataset instance
+	var dataset extensionv1beta1.Dataset
+	if err := r.Get(ctx, req.NamespacedName, &dataset); err != nil {
+		r.Log.Errorf("unable to fetch Dataset: %v", err)
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Check if dataset.spec.datasetmetadata.subsets is not empty
+	if !isSubsetInfoValid(dataset.Spec.DatasetMetadata.DatasetInfo.Subsets) {
+		// If subsets are not valid, set dataset status to UNREADY and return
+		if dataset.Status == nil {
+			dataset.Status = &extensionv1beta1.DatasetStatus{}
+		}
+		dataset.Status.State = extensionv1beta1.DatasetUnready
+		if err := r.Status().Update(ctx, &dataset); err != nil {
+			r.Log.Errorf("unable to update Dataset status: %v", err)
+			return ctrl.Result{}, err
+		}
+	} else {
+		if dataset.Status == nil {
+			dataset.Status = &extensionv1beta1.DatasetStatus{}
+		}
+		dataset.Status.State = extensionv1beta1.DatasetReady
+		if err := r.Status().Update(ctx, &dataset); err != nil {
+			r.Log.Errorf("unable to update Dataset status: %v", err)
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Fetch the DataPlugin instance used by the dataset
+	var dataPlugin extensionv1beta1.DataPlugin
+	if dataset.Spec.DatasetMetadata.Plugin.LoadPlugin {
+		dataPluginName := dataset.Spec.DatasetMetadata.Plugin.Name
+		if err := r.Get(ctx, types.NamespacedName{
+			Namespace: config.GetDatatunerxSystemNamespace(),
+			Name:      dataPluginName,
+		}, &dataPlugin); err != nil {
+			r.Log.Errorf("unable to fetch DataPlugin: %v", err)
+			return ctrl.Result{}, client.IgnoreNotFound(err)
+		}
+
+		// Build the path to the plugin YAML file
+		pluginPath := filepath.Join("plugins", dataPlugin.Spec.DatasetClass, dataPlugin.Spec.Provider, "plugin.yaml")
+		// Apply the plugin YAML file
+		if err := r.applyYAML(ctx, pluginPath, &dataset); err != nil {
+			r.Log.Errorf("unable to apply plugin YAML %v: %v", pluginPath, err)
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Successful reconciliation, no need to requeue
+	return ctrl.Result{}, nil
+}
+
+// isSubsetInfoValid checks if dataset has at least one valid subset with 'train' and 'test' fields
+func isSubsetInfoValid(subsets []extensionv1beta1.Subset) bool {
+	for _, subset := range subsets {
+		// Check if subset has 'train' and 'test' fields
+		if subset.Splits.Train.File != "" && subset.Splits.Test.File != "" {
+			return true
+		}
+	}
+
+	// No valid subset found
+	return false
+}
+
+// applyYAML reads a YAML file, replaces placeholders with environment variable values, and applies its content to the Kubernetes cluster
+func (r *DataPluginReconciler) applyYAML(ctx context.Context, path string, dataset *extensionv1beta1.Dataset) error {
+
+	r.Log.Infof("Applying plugin YAML %v", path)
+	// Read the YAML file content
+	yamlFile, err := os.ReadFile(path)
+	if err != nil {
+		r.Log.Errorf("unable to read plugin YAML file: %v", err)
+		return err
+	}
+
+	// Convert the file content to a string
+	yamlStr := string(yamlFile)
+
+	// Replace placeholders with environment variable values and run-time parameters defined in the dataset
+	replacedYamlStr, err := r.replacePlaceholders(yamlStr, dataset)
+	if err != nil {
+		r.Log.Errorf("unable to replace placeholders in YAML: %v", err)
+		return err
+	}
+
+	// Convert the updated YAML string back to a byte slice
+	yamlFile = []byte(replacedYamlStr)
+
+	// Decode the YAML into an unstructured.Unstructured object
+	decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	unstructuredObj := &unstructured.Unstructured{}
+	_, _, err = decUnstructured.Decode(yamlFile, nil, unstructuredObj)
+	if err != nil {
+		r.Log.Errorf("unable to decode YAML into Unstructured: %v", err)
+		return err
+	}
+
+	// Set the namespace and owner reference
+	unstructuredObj.SetNamespace(dataset.GetNamespace())
+	if err := ctrl.SetControllerReference(dataset, unstructuredObj, r.Scheme); err != nil {
+		r.Log.Errorf("unable to set controller reference: %v", err)
+		return err
+	}
+
+	// Apply the unstructured object using the client
+	if err := r.applyClient(ctx, unstructuredObj); err != nil {
+		r.Log.Errorf("unable to apply Unstructured object: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+// replacePlaceholders replaces a specific placeholder in the YAML file with the value from an environment variable
+func (r *DataPluginReconciler) replacePlaceholders(yamlStr string, dataset *extensionv1beta1.Dataset) (string, error) {
+
+	// Parameters holding the unmarshaled parameters
+	var parameters map[string]interface{}
+
+	// Unmarshal the parameters
+	err := json.Unmarshal([]byte(dataset.Spec.DatasetMetadata.Plugin.Parameters), &parameters)
+	if err != nil {
+		r.Log.Errorf("unable to unmarshal plugin parameters: %v", err)
+		return "", err
+	}
+
+	// Add the required fields defined in the plugin standard to parameters
+	parameters["completeNotifyUrl"] = config.GetCompleteNotifyURL()
+
+	// Replace the value in template yaml
+	replacedYamlStr, err := parser.ReplaceTemplate(yamlStr, parameters)
+	if err != nil {
+		r.Log.Errorf("unable to replace placeholders in YAML: %v", err)
+		return "", err
+	}
+
+	return replacedYamlStr, nil
+}
+
+// applyClient applies or updates the given unstructured object in the cluster using the client
+func (r *DataPluginReconciler) applyClient(ctx context.Context, obj *unstructured.Unstructured) error {
+	// First, try to get the resource, if it exists, update it
+	existing := &unstructured.Unstructured{}
+	existing.SetGroupVersionKind(obj.GroupVersionKind())
+	err := r.Get(ctx, client.ObjectKey{Name: obj.GetName(), Namespace: obj.GetNamespace()}, existing)
+	if err != nil && !errors.IsNotFound(err) {
+		r.Log.Errorf("unable to get existing resource: %v", err)
+		return err
+	}
+
+	if err == nil {
+		// Resource exists, update it
+		obj.SetResourceVersion(existing.GetResourceVersion())
+		if err := r.Update(ctx, obj); err != nil {
+			r.Log.Errorf("unable to update resource: %v", err)
+			return err
+		}
+		r.Log.Info("resource updated successfully")
+	} else {
+		// Resource does not exist, create it
+		if err := r.Create(ctx, obj); err != nil {
+			r.Log.Errorf("unable to create resource: %v", err)
+			return err
+		}
+		r.Log.Info("resource created successfully")
+	}
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *DataPluginReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&extensionv1beta1.Dataset{}).
+		WithEventFilter(predicate.Funcs{
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				oldDataset, ok := e.ObjectOld.(*extensionv1beta1.Dataset)
+				if !ok {
+					return false
+				}
+
+				newDataset, ok := e.ObjectNew.(*extensionv1beta1.Dataset)
+				if !ok {
+					return false
+				}
+				// Only when dataset.spec.datasetmetadata.plugin changes, it returns true, indicating that it needs to be processed
+				return oldDataset.Spec.DatasetMetadata.Plugin != newDataset.Spec.DatasetMetadata.Plugin
+			},
+		}).
+		Complete(r)
+}
